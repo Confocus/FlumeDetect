e85cd275456db50bca7798694f0b8909da016110
Confocus <359904792@qq.com>
2017-09-22 15:28:09 +0800

    finish 1 regex

--- flume-custom/src/test/java/org/apache/flume/custom/RegexTest.java
+++ flume-custom/src/test/java/org/apache/flume/custom/RegexTest.java
@@ -7,14 +7,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexTest{
-	private static String regex = "(/\\*!?|"	//"?"represents non-greedy mode.
-			+ "\\*/|[';]--|--[\\s\\r\\n\\v\\f]|(?:--[^-]*?-)|([^\\-&])#.*?[\\s\\r\\n\\v\\f]|;?\\x00)";     
-	private static String r = "/\\*!?";
-	private static String str = "/*!";
+	
+	private static String regex = "(/\\*!?|"	//eg. "/*!?"			 "?"represents non-greedy mode.
+			+ "\\*/|"							//eg. "*/"
+			+ "[';]--|"							//eg. ";--"			     "[]"represents a set
+			+ "--[\\s\\r\\n\\v\\f]|"			//eg. "--	"			
+			+ "(?:--[^-]*?-)|"					//eg. "--4-"			 "?:" means non acquisition matching; "^" means starting position;"-" means a non- character. 
+			+ "([^\\-&])#.*?[\\s\\r\\n\\v\\f]|" //eg. "a#c	"			 "^" means exclusion.	
+			+ ";?\\x00)";     
+	
+	private static String r = ";?\\x00";
+	private static String str = "/*";
 	
 	public static void main(String[] args){
 		
-		if(RegexMatch.match(r, str)){
+		if(RegexMatch.match(regex, str)){
 			System.out.println("attack.");
 		}
 		else{
